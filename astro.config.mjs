2229857df630378f008ed9a65cb8e957e2669740
DevvyyXYZ <devvyydev@protonmail.com>
2025-02-06 06:43:27 +0000

    finnished

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,16 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
+import tailwindcss from '@tailwindcss/vite';
+
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://devvyyxyz.github.io/blog/',
-	integrations: [mdx(), sitemap()],
+  site: 'https://devvyyxyz.github.io/blog/',
+  integrations: [mdx(), sitemap(), icon()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
