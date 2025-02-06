4d2db7e8a4b04c180f5ab472960f7452d9afb99b
DevvyyXYZ <devvyydev@protonmail.com>
2025-02-06 07:17:11 +0000

    very good changes to structure

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,12 +7,28 @@ import tailwindcss from '@tailwindcss/vite';
 
 import icon from 'astro-icon';
 
+import sentry from '@sentry/astro';
+import spotlightjs from '@spotlightjs/astro';
+
+import robotsTxt from 'astro-robots-txt';
+
+import purgecss from 'astro-purgecss';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://devvyyxyz.github.io/blog/',
-  integrations: [mdx(), sitemap(), icon()],
+  integrations: [
+    mdx(),
+    sitemap(),
+    icon(),
+    sentry(),
+    spotlightjs(),
+    robotsTxt(),
+    purgecss(),
+  ],
 
   vite: {
     plugins: [tailwindcss()],
   },
 });
+
